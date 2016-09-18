31089480627860ed3f2355ba9a8ce8ae05d1355a
George Saieed <saieed.george0@gmail.com>
2016-09-18 00:20:47 -0400

    add token

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAYZBZAdMZAGp0BAKv25CbWJ8811r8s9XoZCFH8jZC5QAmfc3dEX7ZCZCvEsv3HEz3yFzxIM1a5gAJqqZBPav0zwL5Yrld57RGP89Cnn49AiRSZCF67bzBAu8ZCWTdKiNYxhNUZCZCM4PQcKt7ZBthS8qnnIcufBIZCAXCyyt1zUPLIGZBNigZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAH4JBgsCF8BANtkFwmdfVbHrJg1ZB8NYKulhLCx4VqHiJZCrPeZB0YLFFZAHOCyfoNGLUbRUvCpeZAfq18ZCj1me5Jm4MUnhOWh4C7aY8CSPWYwlZBjFNQdExJMYWXVTgCpPftkE8Uzz6PmU29bua0EiEDDFAInRyOrQq7ra5nigZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
