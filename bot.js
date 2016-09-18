5b626aa77e80cc53dfba3f09cad79aa520f83f2a
George Saieed <saieed.george0@gmail.com>
2016-09-17 21:26:33 -0400

    start parsing

--- bot.js
+++ bot.js
@@ -39,6 +39,7 @@ var read = function (sender, message, reply) {
 		reply(sender, message)
 	} 
     else if (message === "duh") {
+        reply(sender, "blahhh")
         request('http://www.modulus.io', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body); // Show the HTML for the Modulus homepage.
