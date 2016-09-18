74fba429f1f38dc6f40718accb799a8d09d1b09d
George Saieed <saieed.george0@gmail.com>
2016-09-17 22:40:23 -0400

    i hate you

--- bot.js
+++ bot.js
@@ -607,7 +607,7 @@ var read = function (sender, message, reply) {
                     var majorwords = listomajors[amajor].name.toString().toLowerCase().split(" ")
                     for(var i = 0; i < majorwords.length; i++) {
                         if (message.toLowerCase().indexOf(majorwords[i]) > -1){
-                            var ha = listomajors[amajor].name.toString() + ", or, " + listomajors[amajor].course.toString() + ", "
+                            var ha = listomajors[amajor].name.toString() + ", or, Course " + listomajors[amajor].course.toString() + ", "
                             if ((listomajors[amajor].ismajor.toString() == "1") && (listomajors[amajor].isminor.toString() == "1")) {
                                 ha += "is offered as a both a major and a minor."
                             }
